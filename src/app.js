366c7cb6c691f82987fa60b8625a7e65b5bd01bd
Matheus Leite <matheusjoselfm@gmail.com>
2020-02-26 10:48:17 -0300

    Adicionando rota para listagem dos provedores e expondo rota para arquivos estaticos

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import path from 'path';
 import routes from './routes';
 
 import './database';
@@ -16,6 +16,10 @@ class App {
 
     middlewares () {
         this.server.use(express.json()); // Receber requests do tipo JSON
+        this.server.use(
+            '/files',
+            express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+        );
     }
 
     routes () {
