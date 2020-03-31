79af70de81b9e32f2d9411a5a6130d641c355579
Matheus Leite <matheusjoselfm@gmail.com>
2020-03-30 21:53:31 -0300

    Ajustes no backend para integracao com projeto web

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -2,6 +2,7 @@ import jwt from 'jsonwebtoken';
 import * as Yup from 'yup';
 
 import User from '../models/User';
+import File from '../models/File';
 
 import authConfig from '../../config/auth';
 
@@ -21,9 +22,14 @@ class SessionController {
         const { email, password } = req.body;
 
         const user = await User.findOne({
-           where: { 
-               email
-            } 
+            where: { email },
+            include: [
+                {
+                    model: File,
+                    as: 'avatar',
+                    attributes: ['id', 'url', 'path'],
+                }
+            ]
         });
 
         if (!user) {
@@ -34,13 +40,15 @@ class SessionController {
             return res.status(401).json({ error: "Password does not match" })
         }
 
-        const { id, name } = user;
+        const { id, name, avatar, provider } = user;
         
         return res.json({
             user: {
                 id,
                 name,
                 email,
+                avatar,
+                provider,
             },
             token: jwt.sign({ id }, authConfig.secret, {
                 expiresIn: authConfig.expiresIn,
