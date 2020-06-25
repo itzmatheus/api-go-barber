865a46c1581e3ee9c7ae9c7aa6186a3bc7ade3ea
Matheus Leite <matheusjoselfm@gmail.com>
2020-06-24 21:28:49 -0300

    Adicionando estrutura de cache

---
+++ src/lib/Cache.js
@@ -0,0 +1,32 @@
+import Redis from 'ioredis';
+
+class Cache {
+  constructor() {
+    this.redis = new Redis({
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      keyPrefix: 'cache:',
+    });
+  }
+
+  async set(key, value) {
+    return this.redis.set(
+      key,
+      JSON.stringify(value),
+      'EX',
+      process.env.REDIS_CACHE_TIME_SEC
+    );
+  }
+
+  async get(key) {
+    const cached = await this.redis.get(key);
+
+    return cached ? JSON.parse(cached) : null;
+  }
+
+  invalidate(key) {
+    return this.redis.del(key);
+  }
+}
+
+export default new Cache();
